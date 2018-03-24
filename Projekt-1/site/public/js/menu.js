86d624c40ac6c2d124673ac984aeeb1588703295
Louie Janbro <louie.janbro@itggot.se>
2018-03-24 13:28:56 +0100

    fixade så att menyn fungerar igen

--- Projekt-1/site/public/js/menu.js
+++ Projekt-1/site/public/js/menu.js
@@ -1,5 +1,5 @@
 function toggleMenu(){
-    var element = document.querySelector(".menu");
+    var element = document.querySelector(".profmenu");
     element.classList.toggle("show");
 
     element = document.querySelector("main");
